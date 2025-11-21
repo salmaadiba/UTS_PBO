53e4aa340977234f98ce6d2d914579ee2077e977
salmaadiba <salmaadiba02@gmail.com>
2025-11-21 21:39:53 +0700

    Add files via upload

---
+++ Pertemuan 5/AlatMusik/src/alatmusik/AlatMusik.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package alatmusik;
+
+/**
+ *
+ * @author salma
+ */
+public class AlatMusik {
+    public void bunyi() {
+        System.out.println("Alat musik mengeluarkan bunyi...");
+    }
+}
---
+++ Pertemuan 5/AlatMusik/src/alatmusik/Gitar.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package alatmusik;
+
+/**
+ *
+ * @author salma
+ */
+public class Gitar extends AlatMusik {
+    @Override
+    public void bunyi() {
+        System.out.println("Gitar berbunyi: jrengg... jrengg...");
+    }
+}
